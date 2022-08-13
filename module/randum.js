9061943a8d88b41cb2a802f8deff1a32f119cea2
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-13 23:45:53 +0530

    update randum.js

--- module/randum.js
+++ module/randum.js
@@ -1,21 +1,28 @@
 const short = require('./short.js');
 const codes = require('../redirect/url.js');
-const run = require('../server.js');
+var { connectDb, insertDb, collDb, findDb } = require('../db.js');
+/*let ru = require('../app.js');*/
+
 function rand(url, out) { 
     var digits = '0123456789'; 
-    let number = ''; 
+    let number = '';
     for (let i = 0; i < 6; i++ ) { 
         number += digits[Math.floor(Math.random() * 10)]; 
     }
     for (let i = 0; i < codes.length; i++) {
-      if (codes[i] == number) {
-        rand();
+      if (!url) {
+        out('short');
       } else {
+      if (codes[i] === number) {
+        rand(url, out);
+      } else {
+        /*ru.runner(number);*/
         codes.push(number);
-        run();
+        //insertDb('paths', codes);
         short(url, number);
         out(number);
         return number;
+      }
       }
     }
 } 
