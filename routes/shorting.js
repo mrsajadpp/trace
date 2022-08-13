95eed75402b70fdc0be440ba32a3be9104903425
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-13 12:07:50 +0530

    Create shorting.js

---
+++ routes/shorting.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const short = require('../module/short.js');
+const rand = require('../module/randum.js');
+let codes = require('../redirect/url.js');
+
+router.post('/', function(req, res, next) {
+  function out(number) {
+    res.render('shorted', { title: 'Url shorter - Trace', description: 'Short you long url to small.', num: number });
+  }
+  let url = req.body.url;
+  if (url.startsWith('http')) {
+    rand(url, out);
+  } else {
+    rand('http://'+url, out);
+  }
+});
+
+module.exports = router;
