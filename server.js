8383118da5def9358dd1c79a48c2aa9434511237
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-14 09:30:37 +0530

    update server.js

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ function listen(port) {
   });
 }
 //Redirecting 
-function run() {
-  for (let i = 0; i < codes.length; i++) {
-    app.get('/'+codes[i], (req, res) => {
-      res.status(200).sendFile(path.join(__dirname, 'redirect/'+codes[i]+'.html'));
+function run(number) {
+    app.get('/u/'+number, (req, res) => {
+      res.status(200).sendFile(path.join(__dirname, 'redirect/'+number+'.html'));
     });
-  }
 }
-/*const port = 3002;
-listen(port);*/
+const port = 3002;
+listen(port);
 
-module.exports = listen;
-module.exports = run;
+module.exports = { 
+  listen,
+  run
+};
