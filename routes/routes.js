7c1d1574e6c713ed27c96254de4ba86e1ce85dea
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-08-14 14:32:12 +0530

    Create routes.js

---
+++ routes/routes.js
@@ -0,0 +1,46 @@
+let fs = require('fs');
+let path = require('path');
+let app;
+function data(ap) {
+  app = ap;
+}
+const rand = require('../modules/randum.js');
+let codes = require('../redirect/url.js');
+function index(express, app, fs, path) {
+  app.get('/', function(req, res) {
+      res.render('index', { title: 'Home - Trace', description: 'Generate your own qr, Capture a website with url, Short your links - Trace' }); 
+    });
+}
+function short(express, app, fs, path) {
+    app.get('/short', function(req, res) {
+      res.render('short', { title: 'Url shorter - Trace', description: 'Short you long url to small.' }); 
+    });
+  }
+  function shorting(express, app, fs, path) {
+    app.post('/shorting', function(req, res) {
+    function out(number) {
+       res.render('shorted', { title: 'Url shorter - Trace', description: 'Short you long url to small.', num: number });
+    }
+    let url = req.body.url;
+    if (!url) {
+       res.render('short', { title: 'Url shorter - Trace', description: 'Short you long url to small.'});
+    } else {
+      if (url.startsWith('http')) {
+         rand(url, out, run);
+      } else {
+         rand('https://'+url, out, run);
+      }
+  }
+ });
+}
+function run(number) {
+  app.get('/'+number, (req, res) => {
+     res.sendFile(path.join(__dirname, '../redirect/'+number+'.html'));
+  });
+}
+module.exports = {
+  index,
+  short,
+  shorting,
+  data
+}
